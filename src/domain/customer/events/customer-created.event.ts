bb96bd78b53679392ce5c0d22ce1f173ec19cc45
pcechetto <pedrocechetto@hotmail.com>
2024-08-15 16:25:38 -0300

    project created

---
+++ src/domain/customer/events/customer-created.event.ts
@@ -0,0 +1,14 @@
+import EventInterface from "../../shared/event.interface";
+
+export default class CustomerCreatedEvent implements EventInterface {
+  dateTimeOcurred: Date;
+  eventData: {
+    id: string;
+    name: string;
+  };
+
+  constructor(eventData: { id: string; name: string }) {
+    this.dateTimeOcurred = new Date();
+    this.eventData = eventData;
+  }
+}
