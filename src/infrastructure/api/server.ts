840d788750e04251f4238910ba24d0fe06b5585b
pcechetto <pedrocechetto@hotmail.com>
2024-08-20 14:12:34 -0300

    configurando express

---
+++ src/infrastructure/api/server.ts
@@ -0,0 +1,7 @@
+import { configDotenv } from "dotenv";
+import { app } from "./express";
+
+configDotenv();
+const port: number = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
